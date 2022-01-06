4e8363b8061b72d5c2548e19400c8b000a996c71
xieeee652 <peterxie0811@gmail.com>
2022-01-06 10:54:39 +0800

    108360106

--- app/src/main/java/com/example/final_test/LoginActivity.java
+++ app/src/main/java/com/example/final_test/LoginActivity.java
@@ -49,7 +49,7 @@ public class LoginActivity extends AppCompatActivity {
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //  startActivity(new Intent(LoginActivity.this, SignInActivity.class));
+                startActivity(new Intent(LoginActivity.this, SignInActivity.class));
             }
         });
 
